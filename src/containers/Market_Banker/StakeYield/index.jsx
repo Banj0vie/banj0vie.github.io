633205f9d4fda985f0c17d04c56f0c949a81bce6
davidlong0526 <davidlong.pro0526@gmail.com>
2025-09-16 06:22:45 -0700

    Add stake and unstake button

--- src/containers/Market_Banker/StakeYield/index.jsx
+++ src/containers/Market_Banker/StakeYield/index.jsx
@@ -3,10 +3,18 @@ import "./style.css";
 import BaseButton from "../../../components/buttons/BaseButton";
 
 const StakeYield = () => {
+  const onStake = () => {
+
+  }
+
+  const onUnstake = () => {
+
+  }
+  
   return (
     <div className="stake-yield">
       <div className="stake-unstake-buttons">
-        <BaseButton></BaseButton>
+        <BaseButton label="Stake" onClick={onStake}></BaseButton>
         <BaseButton></BaseButton>
       </div>
     </div>
