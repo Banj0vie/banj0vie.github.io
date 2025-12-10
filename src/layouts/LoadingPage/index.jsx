8c59a62c6334bed2d8f7a4429da8f55c17cbb2bf
davidlong0526 <davidlong.pro0526@gmail.com>
2025-12-09 18:07:10 -0800

    updated loading bar

--- src/layouts/LoadingPage/index.jsx
+++ src/layouts/LoadingPage/index.jsx
@@ -5,6 +5,7 @@ const LoadingPage = () => {
   return (
     <div className="loading-page">
       <div className="loading-content">
+        <img src="/images/loading/loading.png" alt="Loading" className="loading-image" />
         <div className="loading-spinner"></div>
       </div>
     </div>
