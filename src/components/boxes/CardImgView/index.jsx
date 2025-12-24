ff7a6999259d46635794c79877e82470d058bb8e
davidlong0526 <davidlong.pro0526@gmail.com>
2025-12-23 19:17:37 -0800

    update cardimage view independently

--- src/components/boxes/CardImgView/index.jsx
+++ src/components/boxes/CardImgView/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import "./style.css";
 
 const CardImgView = ({ className, children, secondary = false, onClick }) => {
-    return <div className={`card-view ${className} ${secondary ? "secondary" : ""}`} onClick={onClick}>
+    return <div className={`card-img-view ${className} ${secondary ? "secondary" : ""}`} onClick={onClick}>
         <img src="/images/profile_bar/avatar_bg.png" alt="avatar-bg" className="card-bg" />
         <div className="card-content">{children}</div>
     </div>
