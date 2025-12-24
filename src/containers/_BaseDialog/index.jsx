9a06ca7d827a8dd012aa3d012142a60c3da4d736
davidlong0526 <davidlong.pro0526@gmail.com>
2025-12-23 20:52:10 -0800

    fix multi modal opening

--- src/containers/_BaseDialog/index.jsx
+++ src/containers/_BaseDialog/index.jsx
@@ -11,18 +11,18 @@ const BaseDialog = ({ className = "", title, onClose, children, header = null })
           className="modal"
           onClick={(e) => e.stopPropagation()}
         >
-          <div className="modal-close" onClick={onClose}></div>
           <img className="modal-left-top-image" src="/images/dialog/left-top.png" alt="left-top-image" />
           <img className="modal-bottom-center-image" src="/images/dialog/bottom-center.png" alt="bottom-center-image" />
           <img className="modal-bottom-center-emerald" src="/images/dialog/bottom-center-emerald.png" alt="bottom-center-emerald-image" />
           <div className="modal-body">
             {children}
-          </div>
-          <div className="modal-header">
-            <img className="modal-header-image" src="/images/dialog/top-baseheader.png" alt="header" />
-            {header && <img className="modal-header-additional-image" src={header} alt="header" />}
-            <img className="modal-header-label-scroll" src="/images/dialog/label-scroll.png" alt="header" />
-            <span className="modal-header-title">{title}</span>
+            <div className="modal-header">
+              <img className="modal-header-image" src="/images/dialog/top-baseheader.png" alt="header" />
+              {header && <img className="modal-header-additional-image" src={header} alt="header" />}
+              <img className="modal-header-label-scroll" src="/images/dialog/label-scroll.png" alt="header" />
+              <span className="modal-header-title">{title}</span>
+            </div>
+            <div className="modal-close" onClick={onClose}></div>
           </div>
         </div>
       </div>
